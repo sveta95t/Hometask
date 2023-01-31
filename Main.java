a3b89c0a6eb8c3cc9caac47f40deda745e361954
Svetlana <123013190+sveta95t@users.noreply.github.com>
2023-01-31 21:33:29 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("привет всем!");
+        System.out.print("Я Светлана!");
+        System.out.println("мне нравятся стихи C.Есенина");
+        System.out.println("ты меня не любишь не жалеешь");
+        System.out.println("разве я немного некрасив");
+        System.out.println("не смотря в глаза от страсти млеешь");
+    }
+}
